6b7474884bd33afde8917e93892c9ae531e44604
Ismael Sabri <hisme14@gmail.com>
2022-09-16 11:28:51 +0200

    Initial Commit

---
+++ CodableCoreDataEntitiesSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodableCoreDataEntitiesSample
+//
+//  Created by Ismael Sabri Pérez on 16/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
